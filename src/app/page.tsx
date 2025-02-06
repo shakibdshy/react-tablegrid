4d3e38f26293c711162b499d2999b5a9074a79c1
shakibdshy <shakibdshy@gmail.com>
2025-02-07 00:08:34 +0600

    Update Home Page: Modify Table Example Imports and Rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,31 @@
-// import BasicTable from "@/components/examples/basic-table";
-// import CustomizedTable from "@/components/examples/customized-table";
-// import HeaderGroupsTable from "@/components/examples/header-groups";
-// import BasicSearch from "@/components/examples/basic-search";
+import BasicTable from "@/components/examples/basic-table";
+import BasicSearch from "@/components/examples/basic-search";
+import HeaderGroupsTable from "@/components/examples/header-groups";
 // import FuzzySearchFilter from "@/components/examples/fuzzy-search-filter";
-// import ColumnVisibilityTable from "@/components/examples/column-visibility-table";
-import VirtualizedTable from "@/components/examples/virtualized-table";
-// import ColumnPinningTable from "@/components/examples/column-pinning-table";
-// import CustomizedTable from "@/components/examples/customized-table";
-// import ColumnResizingTable from "@/components/examples/column-resizing-table";
-// import PaginationExample from "@/components/examples/pagination";
-// import ToggleColumnPinningTable from "@/components/examples/toggle-column-pinning-table";
 // import NestedTableExample from "@/components/examples/nested-table";
+// import ToggleColumnPinningTable from "@/components/examples/toggle-column-pinning-table";
+// import ColumnPinningTable from "@/components/examples/column-pinning-table";
+// import ColumnVisibilityTable from "@/components/examples/column-visibility-table";
+// import VirtualizedTable from "@/components/examples/virtualized-table";
+// import ColumnResizingTable from "@/components/examples/column-resizing-table";
+import CustomizedTable from "@/components/examples/customized-table";
+import PaginationExample from "@/components/examples/pagination";
 
 export default function Home() {
   return (
     <main className="min-h-screen p-4">
-      {/* <BasicTable /> */}
-      {/* <BasicSearch /> */}
-      {/* <HeaderGroupsTable /> */}
+      <BasicTable />
+      <BasicSearch />
+      <HeaderGroupsTable />
       {/* <FuzzySearchFilter /> */}
-      {/* <ColumnPinningTable /> */}
-      {/* <ToggleColumnPinningTable /> */}
-      {/* <ColumnVisibilityTable /> */}
-      {/* <CustomizedTable /> */}
-      <VirtualizedTable />
-      {/* <ColumnResizingTable /> */}
-      {/* <PaginationExample /> */}
       {/* <NestedTableExample /> */}
+      {/* <ToggleColumnPinningTable /> */}
+      {/* <ColumnPinningTable /> */}
+      {/* <ColumnVisibilityTable /> */}
+      {/* <VirtualizedTable /> */}
+      {/* <ColumnResizingTable /> */}
+      <CustomizedTable />
+      <PaginationExample />
 
     </main>
   );
